5c603035a61e1e9fefbcb0d61adea238653804f0
lwalden <lwalden77@gmail.com>
2014-04-03 15:20:07 -0500

    cleaned up code to follow rules of javascript

--- Day4/letterCountI.js
+++ Day4/letterCountI.js
@@ -4,13 +4,18 @@
     var words = sen.toLowerCase().split(" "),  //makes array of words
         dupCounter,
         dups = [],
-        allDups = [];
+        allDups = [],
+        largest = 0,
+        largestIndex = 0,
+        i,
+        j,
+        h;
 
-    for (var i = 0; i < words.length; i++) { //loops through words 
+    for (i = 0; i < words.length; i++) { //loops through words 
         dups = [];
-        for (var j = 0; j < words[i].length; j++) { //loops through the letters in each word
+        for (j = 0; j < words[i].length; j++) { //loops through the letters in each word
             dupCounter = 1;
-            for (var h = (j + 1) ; h < words[i].length; h++) {
+            for (h = (j + 1) ; h < words[i].length; h++) { //checks all leters after letter j for duplicates
                 if (words[i][j] === words[i][h]) {
                     dupCounter += 1;
                 }
@@ -19,8 +24,7 @@
         }
         allDups[i] = dups.sort(function (a, b) { return a - b }).pop();
     }
-    var largest = 0;
-    var largestIndex = 0;
+    
     for (i = 0; i < allDups.length; i++) {
         if (allDups[i] > largest) {
             largest = allDups[i];
