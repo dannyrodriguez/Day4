6a872f367b27ecf511f9981e9cd1ad7117d74d57
lwalden <lwalden77@gmail.com>
2014-04-03 12:39:47 -0500

    added file

---
+++ Day4/JavaScript1.js
@@ -0,0 +1,27 @@
+﻿var combine = function(a) {
+    "use strict";
+
+    var all = [],
+        i,
+        fn = function (n, src, got, all) {
+            var j;
+
+            if (n === 0) {
+                if (got.length > 0) {
+                    all[all.length] = got;
+                }
+                return;
+            }
+            for (j = 0; j < src.length; j++) {
+                fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
+            }
+            return;
+        }
+
+    for (i=0; i < a.length; i++) {
+        fn(i, a, [], all);
+    }
+    all.push(a);
+    return all;
+}
+
